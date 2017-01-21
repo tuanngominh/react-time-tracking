d891e942ea7499cf283061cbb7fc2cdc0574721b
Tuan Ngo <tuan@trexanhlab.com>
2017-01-21 12:44:52 +0700

    refactor: move routes catalog to separate file: routes.js

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,34 +1,15 @@
-import React from 'react'
-import App from './App'
-import Profile from './Profile'
-import Reports from './Reports'
-import TimeTracker from './TimeTracker'
+import React, {PropTypes} from 'react'
 
-import Login from './auth/Login'
-import Logout from './auth/Logout'
+import {Router} from 'react-router'
+import routes from '../routes'
 
-import {Router, Route, IndexRoute} from 'react-router'
-
-import { useRouterHistory } from 'react-router'
-import { createHistory } from 'history'
-
-// export const basename = '/vivid-trace'
-export const basename = ''
-
-const browserHistory = useRouterHistory(createHistory)({
-  basename: basename
-})
-
-const Root = ({store}) => (
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={TimeTracker} />
-      <Route path='/reports' component={Reports} />
-      <Route path='/profile' component={Profile} />
-      <Route path='/login' component={Login} />
-      <Route path='/logout' component={Logout} />
-    </Route>
+const Root = ({history}) => (
+  <Router history={history} routes={routes}>
   </Router>
 )
+
+Root.propTypes = {
+  history: PropTypes.object.isRequired
+}
 
 export default Root
