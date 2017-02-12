8ac46223b12c5dfd0e5ead62d330ef507c8ba515
tuanngominh <tuan@trexanhlab.com>
2017-02-12 23:03:10 +0700

    filter startdate on firebase

--- src/actions/report.js
+++ src/actions/report.js
@@ -4,35 +4,39 @@ import {actionStart, actionSuccess} from './utils/template'
 
 // Fetch report data
 export const fetch = (uid, text, startDate, endDate) => {
-  console.log('fetch')
-  console.log({uid, text, startDate, endDate})
   return function(dispatch) {
     dispatch(actionStart(types.REPORT_FETCH))
+
     const ref = firebase.database().ref('timeEntries/' + uid)
-    //filter by date on serverside
-    ref.orderByChild('startDate').startAt(startDate.toJSON()).endAt(endDate.toJSON())
-    return new Promise(function(resolve, reject){
-      ref.once('value', function(snapshot){
-        let entries = {}
-        snapshot.forEach(function(childSnapshot){
-          var childKey = childSnapshot.key
-          var childData = childSnapshot.val()
-          //filter text on clientside
-          if (text) {
-            if (childData.text.indexOf(text) !== -1) {
-              entries[childKey] = childData
-            }            
-          } else {
-            entries[childKey] = childData
+      .orderByChild('startTime')
+        .startAt(startDate.getTime())
+        .endAt(endDate.getTime())
+
+    return new Promise(function(resolve, reject){      
+      ref.on('value', function(snapshot){
+        let entries = snapshot.val()
+        let filteredEntries = {}
+
+        for (let key in entries) {
+          if (entries.hasOwnProperty(key)) {
+            entries[key].startTime = new Date(entries[key].startTime)
+            entries[key].endTime = new Date(entries[key].endTime)
+            //filter text on clientside
+            if (text) {
+              if (entries[key].text.indexOf(text) !== -1) {
+                filteredEntries[key] = entries[key]
+              }            
+            }
           }
-        })
+        }
+
+        if (text) {
+          entries = filteredEntries
+        }
 
         dispatch(actionSuccess(types.REPORT_FETCH, {payload: {
           entries
         }}))
-
-        console.log('entries')
-        console.log(entries)
 
         resolve()
       })
