dfef9e9d92d45513799dc701d83347259c3f4bc9
tuanngominh <tuan@trexanhlab.com>
2017-02-13 15:07:16 +0700

    show loading during load report data

--- src/containers/Report.js
+++ src/containers/Report.js
@@ -6,6 +6,7 @@ import ReportFilter from './Report/Filter'
 import ReportEntryList from './Report/EntryList'
 import BarChart from './Report/BarChart'
 import Paper from 'material-ui/Paper'
+import RefreshIndicator from 'material-ui/RefreshIndicator'
 
 const style = {
   padding: 10,
@@ -19,6 +20,7 @@ class Report extends Component {
     startDate.setDate(startDate.getDate() - 7 )
     const endDate = new Date()
 
+    const hasData = this.props.entries && (Object.keys(this.props.entries).length > 0)
     return (
       <div className="row">
         <div className="col-xs-12
@@ -29,10 +31,28 @@ class Report extends Component {
           <Paper style={style} zDepth={1} >
             <ReportFilter startDate={startDate} endDate={endDate}/>
           </Paper>
-          
-          <BarChart labels={this.props.reportBarChartData.labels} data={this.props.reportBarChartData.data} />
-          <ReportEntryList entries={this.props.entries} />
-          
+          {
+            this.props.isFetching
+            ?
+              <div style={{textAlign: 'center', position: 'relative'}}>
+                <RefreshIndicator
+                  size={40}
+                  left={10}
+                  top={0}
+                  status="loading"
+                  style={{display: 'inline-block', position: 'relative'}}
+                />
+              </div>
+            :
+              hasData
+              ?
+                <div>
+                  <BarChart labels={this.props.reportBarChartData.labels} data={this.props.reportBarChartData.data} />
+                  <ReportEntryList entries={this.props.entries} />
+                </div>
+              :
+                <p style={{textAlign: 'center'}}>No time entry found</p>
+          }
         </div>
       </div>
     )    
