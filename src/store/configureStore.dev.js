f61e4321b48407ebc48ba9cdabcb88a4d4a40cd4
tuanngominh <tuan@trexanhlab.com>
2017-02-15 10:49:49 +0700

    remove debug code

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -2,7 +2,7 @@ import thunkMiddleware from 'redux-thunk'
 import reducers from '../reducers'
 import {createStore, applyMiddleware} from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-console.log('dev')
+
 const configureStore = () => {
   const store = createStore(
     reducers, 
