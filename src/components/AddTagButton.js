fbcb2be6559bc194ffb5ac0e2ff3b5bb0f028052
tuanngominh <tuan@trexanhlab.com>
2017-03-02 11:47:22 +0700

    select tag: remove redundant params

--- src/components/AddTagButton.js
+++ src/components/AddTagButton.js
@@ -19,8 +19,8 @@ export class TagItem extends Component {
   }
 
   handleSelectTag = () => {
-    const {onSelectTag, id, name, color} = this.props    
-    onSelectTag(id, name, color)
+    const {onSelectTag, id} = this.props    
+    onSelectTag(id)
   }
 
   render() {
