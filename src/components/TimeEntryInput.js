b9a3fe5c8e2bc0f7279dab97218e368b0f6f12f0
Tuan Ngo <tuannm79@gmail.com>
2020-07-12 11:23:13 +0700

    wip: using moment to show duration

--- src/components/TimeEntryInput.js
+++ src/components/TimeEntryInput.js
@@ -1,13 +1,15 @@
 import React from "react";
+import moment from "moment";
 
 export class TimeEntryInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      start: new Date(),
-      end: new Date(),
+      start: null,
+      end: null,
     };
     this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
   }
 
   start() {
@@ -16,6 +18,16 @@ export class TimeEntryInput extends React.Component {
       end: new Date(),
     });
     this.timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  stop() {
+    this.setState({
+      start: null,
+      end: null,
+    });
+    if (this.timerId) {
+      clearInterval(this.timerId);
+    }
   }
 
   tick() {
@@ -30,21 +42,46 @@ export class TimeEntryInput extends React.Component {
     }
   }
 
+  get durationAsText() {
+    if (this.state.start) {
+      const start = moment(this.state.start);
+      const end = moment(this.state.end);
+      const d = moment.duration(end.diff(start, "second"), "second");
+      if (d.asDays() > 1) {
+        return d.humanize().toString();
+      } else {
+        return (
+          Math.round(d.hours()) +
+          " : " +
+          Math.round(d.minutes()) +
+          " : " +
+          Math.round(d.seconds())
+        );
+      }
+    }
+    return null;
+  }
+
   render() {
-    let duration = "";
-    if (this.state.start !== this.state.end) {
-      duration = (
-        <span>
-          Start: {this.state.start.toString()} - End:{" "}
-          {this.state.end.toString()}
-        </span>
+    let duration = <span></span>;
+    let button = (
+      <button data-testid="start-button" onClick={this.start}>
+        Start
+      </button>
+    );
+    if (this.state.start) {
+      duration = <span data-testid="duration">{this.durationAsText}</span>;
+      button = (
+        <button data-testid="stop-button" onClick={this.stop}>
+          Stop
+        </button>
       );
     }
     return (
       <div>
         <input placeholder="What are you doing?" value={this.props.title} />
-        <button onClick={this.start}>Start</button>
         {duration}
+        {button}
       </div>
     );
   }
--- src/components/TimeEntryInput.test.js
+++ src/components/TimeEntryInput.test.js
@@ -1,17 +1,32 @@
 import { TimeEntryInput } from "./TimeEntryInput";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import React from "react";
 
 test("not start tracking yet should have start button", () => {
   render(<TimeEntryInput />);
-  expect(screen.getByRole("button")).toHaveTextContent("Start");
+  expect(screen.getByTestId("start-button")).toBeInTheDocument();
 });
 
-test("start tracking should return time entry name", () => {});
+test("start tracking should return time entry name", () => {
+  render(<TimeEntryInput />);
+  const button = screen.getByTestId("start-button");
+  fireEvent.click(button);
+  expect(screen.getByTestId("duration")).toBeInTheDocument();
+});
 
-test("during a time entry, there should be stop button", () => {});
+test("during a time entry, there should be stop button", () => {
+  render(<TimeEntryInput />);
+  const button = screen.getByTestId("start-button");
+  fireEvent.click(button);
+  expect(screen.getByTestId("stop-button")).toBeInTheDocument();
+});
 
-test("during a time entry, there should be duration", () => {});
+test("during a time entry, there should be duration", () => {
+  render(<TimeEntryInput />);
+  const button = screen.getByTestId("start-button");
+  fireEvent.click(button);
+  expect(screen.getByTestId("duration")).toBeInTheDocument();
+});
 
 test("during a time entry, click stop button should show start button", () => {});
 
